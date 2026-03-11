e7831510857b12e9d240386ac30b6eb64eeba338
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-11 13:47:12 +0500

    add slidebar and main component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>HarixAi</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
