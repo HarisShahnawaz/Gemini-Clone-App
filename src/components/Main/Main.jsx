a5331de45c6d1e88a28f3618bdf0f081c91bdc25
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-11 16:22:15 +0500

    complete our design portion

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,6 +8,48 @@ const Main = () => {
         <p>HarixAi</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello , Dev.</span>
+          </p>
+          <p>How can i help you today?</p>
+        </div>
+
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Breifly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter A Prompt Here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            HarixAi may display inaccurate info,including about people,so
+            double-check its responces.Your privacy and HarixAi Apps
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
