8badb6032c7c44fbcacb9628d59d1a84763100e5
HarisShahnawaz <harisshahnawaz97@gmail.com>
2026-03-11 20:55:54 +0500

    add gemini key

---
+++ src/config/gemini.js
@@ -0,0 +1,14 @@
+const apiKey = "AIzaSyDymU1rPTeC9RtBdioeJSRHu2oxJYjUXLQ";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
